686fe4a99375a22d3ef3ab83c5678bafe60c4da6
addyosmani <addyosmani@gmail.com>
2012-09-12 11:41:33 +0100

    Adding `warnOn` for generators where necessary.

--- all/index.js
+++ all/index.js
@@ -8,6 +8,8 @@ var Generator = module.exports = function Generator() {
   this.sourceRoot(path.join(__dirname, '../templates'));
 
   this.appname = path.basename(process.cwd());
+
+  this.warnOn('*');
   
   this.hookFor('angular:app', {
     args: ['main']
