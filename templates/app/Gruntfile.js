c5fac777207c2d050b83168ccdda72fbbb92466f
addyosmani <addyosmani@gmail.com>
2012-09-24 15:31:18 +0100

    Fixes #65 - generators now use app/components for bower installs

--- templates/app/Gruntfile.js
+++ templates/app/Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function( grunt ) {
 
     // specify an alternate install location for Bower
     bower: {
-      dir: 'app/scripts/vendor'
+      dir: 'app/components'
     },
 
     // Coffee to JS compilation
