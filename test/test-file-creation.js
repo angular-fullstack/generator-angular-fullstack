921f3f346332b4d5b34b6c675e15c50d53c39bc3
Frederick Ros <frederick.ros@gmail.com>
2012-11-30 18:04:10 +0100

    Added creation of bowerrc and additional dotgiles...
    ... as well as associated tests

---
+++ test/test-file-creation.js
@@ -0,0 +1,27 @@
+var assert = require("assert");
+var Generator = require("../common");
+var grunt = require("grunt");
+var fs = require("fs");
+var path = require("path");
+var assert = require("assert");
+var yeoman = require('yeoman').generators.test;
+
+describe("Angular generator", function() {
+  before(yeoman.before(path.join(__dirname, 'temp')));
+
+  it ("should generate dotfiles", function() {
+    // FIXME: Remove the Gruntfile.js created by the test.before function
+    fs.unlinkSync("Gruntfile.js");
+
+    var g = new Generator();
+    g.run();
+
+    yeoman.assertFile(".bowerrc");
+    yeoman.assertFile(".gitignore");
+    yeoman.assertFile(".editorconfig");
+    yeoman.assertFile(".jshintrc");
+
+  });
+
+});
+
