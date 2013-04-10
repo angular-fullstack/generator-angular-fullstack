3c8daab254ba0b4ff4bbe397547e09dcb04892b9
Samuel Santos <samaxes@gmail.com>
2013-04-11 01:35:43 +0200

    fix(build): add SVG files to Grunt rev task.
    Closes #157

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -235,7 +235,7 @@ module.exports = function (grunt) {
           src: [
             '<%%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+            '<%%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%%= yeoman.dist %>/styles/fonts/*'
           ]
         }
