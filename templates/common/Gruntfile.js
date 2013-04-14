32afa5ad0e185bfaf4192e781163f546915028aa
Sindre Sorhus <sindresorhus@gmail.com>
2013-04-14 14:35:08 +0200

    fix(gen): remove trailing comma in gruntfile.
    Fixes #167

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -225,7 +225,7 @@ module.exports = function (grunt) {
         files: {
           '<%%= yeoman.dist %>/scripts/scripts.js': [
             '<%%= yeoman.dist %>/scripts/scripts.js'
-          ],
+          ]
         }
       }
     },
