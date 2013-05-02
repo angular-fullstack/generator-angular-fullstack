7d9b862c0ad814ec3e270a633b475208d39ef9dc
Robin Böhm <robinboehm@googlemail.com>
2013-05-02 15:52:54 +0200

    feat(decorator): Add prompt if file already exists

--- decorator/index.js
+++ decorator/index.js
@@ -1,30 +1,74 @@
 'use strict';
-var path = require('path');
 var util = require('util');
 var ScriptBase = require('../script-base.js');
-var angularUtils = require('../util.js');
+var fs = require('fs');
 
-
-module.exports = Generator;
-
-function Generator() {
+var Generator = module.exports = function Generator(args, options) {
     ScriptBase.apply(this, arguments);
-    var postFix = "Decorator";
-
-    //TODO: Any better way in yeoman to get this value?
-    var fileName = arguments[0][1];
-    if(fileName === undefined){
-        fileName = this.name+postFix;
-    }
-    else{
-        fileName += postFix;
-    }
-    this.fileName = fileName;
+    this.fileName = this.name;
 }
 
 util.inherits(Generator, ScriptBase);
 
-Generator.prototype.createDecoratorFiles = function createDecoratorFiles() {
-    this.appTemplate('decorator', 'scripts/decorators/' + this.fileName);
-    this.addScriptToIndex('decorators/' + this.fileName);
+Generator.prototype.askForOverwrite = function askForOverwrite() {
+    var cb = this.async();
+
+    // TODO: Any yeoman.util function to handle this?
+    var fileExists = fs.existsSync(this.env.cwd+'/app/scripts/'+buildRelativePath(this.fileName)+".js");
+    if (fileExists) {
+        var prompts = [{
+            name: 'overwriteDecorator',
+            message: 'Would you like to overwrite existing decorator?',
+            default: 'Y/n',
+            warning: 'Yes: Decorator will be replaced..'
+        }];
+
+        this.prompt(prompts, function (err, props) {
+            if (err) {
+                return this.emit('error', err);
+            }
+
+            this.overwriteDecorator = (/y/i).test(props.overwriteDecorator);
+
+            cb();
+        }.bind(this));
+    }
+    else{
+        cb();
+        return;
+    }
 };
+
+Generator.prototype.askForNewName = function askForNewName() {
+    var cb = this.async();
+
+    if(this.overwriteDecorator === undefined || this.overwriteDecorator === true){
+        cb();
+        return;
+    }
+    else{
+        var prompts = new Array();
+        prompts.push({
+            name: 'decortatorName',
+            message: 'Alternative name for the decorator:'
+        });
+
+        this.prompt(prompts, function (err, props) {
+            if (err) {
+                return this.emit('error', err);
+            }
+            this.fileName = props.decortatorName;
+
+            cb();
+        }.bind(this));
+    }
+};
+
+Generator.prototype.createDecoratorFiles = function createDecoratorFiles() {
+    this.appTemplate('decorator', 'scripts/'+buildRelativePath(this.fileName));
+    this.addScriptToIndex(buildRelativePath(this.fileName));
+};
+
+function buildRelativePath(fileName){
+    return 'decorators/' + fileName+"Decorator";
+}
