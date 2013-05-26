c77b83e6a822d98ec374c2d9959b20f0a28cfac6
Robin Böhm <robinboehm@googlemail.com>
2013-05-26 17:41:48 +0200

    style(decorator): add missing spaces

--- templates/javascript/decorator.js
+++ templates/javascript/decorator.js
@@ -2,7 +2,7 @@
 
 angular.module('<%= _.camelize(appname) %>App')
     .config(function ($provide) {
-        $provide.decorator('<%= _.camelize(name) %>', function($delegate){
+        $provide.decorator('<%= _.camelize(name) %>', function ($delegate) {
             // decorate the $delegate
             return $delegate;
         });
