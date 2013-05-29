5b6a4b66dd036dc1bab55e17a5aba8aa3446c95f
Robin Böhm <robinboehm@googlemail.com>
2013-05-29 20:12:51 +0200

    refactor(service-generator): fix break in Angular-Sub-Generator API of service-generator, remove argument --type and create own generators for each type

--- factory/index.js
+++ factory/index.js
@@ -14,7 +14,7 @@ function Generator() {
 util.inherits(Generator, ScriptBase);
 
 Generator.prototype.createServiceFiles = function createServiceFiles() {
-  this.appTemplate('service/service', 'scripts/services/' + this.name);
+  this.appTemplate('service/factory', 'scripts/services/' + this.name);
   this.testTemplate('spec/service', 'services/' + this.name);
   this.addScriptToIndex('services/' + this.name);
 };
