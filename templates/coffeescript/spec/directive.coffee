2753c990dbdc8efc7a5f245868cd10f15080c140
Pascal Hartig <phartig@rdrei.net>
2013-07-26 11:40:56 +0200

    fix(templates): new scope for directive spec
    To mirror the controller spec template, the generated tests for directives now
    mock a new scope instead of working directly on the $rootScope.

--- templates/coffeescript/spec/directive.coffee
+++ templates/coffeescript/spec/directive.coffee
@@ -1,11 +1,16 @@
 'use strict'
 
 describe 'Directive: <%= _.camelize(name) %>', () ->
+
+  # load the directive's module
   beforeEach module '<%= _.camelize(appname) %>App'
 
-  element = {}
+  scope = {}
 
-  it 'should make hidden element visible', inject ($rootScope, $compile) ->
+  beforeEach inject ($controller, $rootScope) ->
+    scope = $rootScope.$new()
+
+  it 'should make hidden element visible', inject ($compile) ->
     element = angular.element '<<%= _.dasherize(name) %>></<%= _.dasherize(name) %>>'
-    element = $compile(element) $rootScope
+    element = $compile(element) scope
     expect(element.text()).toBe 'this is the <%= _.camelize(name) %> directive'
--- templates/javascript/spec/directive.js
+++ templates/javascript/spec/directive.js
@@ -1,13 +1,20 @@
 'use strict';
 
 describe('Directive: <%= _.camelize(name) %>', function () {
+
+  // load the directive's module
   beforeEach(module('<%= _.camelize(appname) %>App'));
 
-  var element;
+  var element,
+    scope;
 
-  it('should make hidden element visible', inject(function ($rootScope, $compile) {
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
     element = angular.element('<<%= _.dasherize(name) %>></<%= _.dasherize(name) %>>');
-    element = $compile(element)($rootScope);
+    element = $compile(element)(scope);
     expect(element.text()).toBe('this is the <%= _.camelize(name) %> directive');
   }));
 });
