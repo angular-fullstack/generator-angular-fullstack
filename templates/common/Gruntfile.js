6f7d17e2a0f1f7f9f8cac3157beb07b82e8cf400
Pascal Hartig <passy@twitter.com>
2013-10-17 14:14:17 +0100

    fix(templates): Gruntfile indentation

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -75,7 +75,7 @@ module.exports = function (grunt) {
       },
       test: {
         options: {
-         port: 9001,
+          port: 9001,
           base: [
             '.tmp',
             'test',
