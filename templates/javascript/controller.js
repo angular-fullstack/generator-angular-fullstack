aa4b66213a38afc61b5c7a721a7f88997b28b94d
Tyler Henkel <tylerhenkel@hotmail.com>
2013-10-31 00:32:48 -0400

    fixed controller to use un-minified template as default

--- templates/javascript/controller.js
+++ templates/javascript/controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('<%= _.camelize(appname) %>App')
-  .controller('<%= _.classify(name) %>Ctrl', ['$scope', '$http', function ($scope, $http) {
+  .controller('<%= _.classify(name) %>Ctrl', function ($scope, $http) {
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
-  }]);
+  });
