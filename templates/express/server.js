bdbca2e3c9e12414c261813b48e56693d93d58cb
Tyler Henkel <tylerhenkel@hotmail.com>
2013-10-31 06:35:35 -0400

    replaced double quotes with single quotes in server.js console log

--- templates/express/server.js
+++ templates/express/server.js
@@ -30,5 +30,5 @@ else {
 app.get('/api/awesomeThings', api.awesomeThings);
 
 http.createServer(app).listen(app.get('port'), function () {
-  console.log("Express server listening on port %d in %s mode", app.get('port'), app.get('env'));
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
