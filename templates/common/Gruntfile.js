8a52a265aa15a0f589109d6cd1dac4ae7dc5a3e9
Eddie Monge <eddie@eddiemonge.com>
2013-12-17 11:12:39 -0800

    fix(app): copy view files to dist folder
    htmlmin was previously copying the view files. Since it now operates
    solely on the dist folder, the views needed to be manually copied

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -281,7 +281,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%%= yeoman.dist %>',
-          src: ['*.html', 'views/*.html'],
+          src: ['*.html', 'views/{,*/}*.html'],
           dest: '<%%= yeoman.dist %>'
         }]
       }
@@ -319,6 +319,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
+            'views/{,*/}*.html',
             'bower_components/**/*',
             'images/{,*/}*.{webp}',
             'fonts/*'
