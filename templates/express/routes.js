4d3b19d880808c69d1ba770f931f15cab4a73812
jay <jay@technocrat.com.au>
2014-01-12 18:02:53 +1100

    Adding Facebook, Twitter and Google authentication routing

--- templates/express/routes.js
+++ templates/express/routes.js
@@ -23,6 +23,44 @@ module.exports = function(app) {
   app.post('/api/session', session.login);
   app.del('/api/session', session.logout);<% } %>
 
+  // Authentication Routes
+  <% if (mongoPassportFacebook) { %>
+  // Setting the Facebook oauth routes
+  app.get('/auth/facebook', passport.authenticate('facebook', {
+          scope: ['email', 'user_about_me'],
+          failureRedirect: '/signin'
+      }), users.signin);
+
+      app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+          failureRedirect: '/signin'
+      }), users.authCallback); <% } %>
+
+  <% if (mongoPassportTwitter) { %>
+  // Setting the Twitter oauth routes
+  app.get('/auth/twitter', passport.authenticate('twitter', {
+          failureRedirect: '/signin'
+      }), users.signin);
+
+      app.get('/auth/twitter/callback', passport.authenticate('twitter', {
+          failureRedirect: '/signin'
+      }), users.authCallback);
+  <% } %>
+
+  <% if (mongoPassportGoogle) { %>
+  // Setting the Google oauth routes
+  app.get('/auth/google', passport.authenticate('google', {
+          failureRedirect: '/signin',
+          scope: [
+              'https://www.googleapis.com/auth/userinfo.profile',
+              'https://www.googleapis.com/auth/userinfo.email'
+          ]
+      }), users.signin);
+
+      app.get('/auth/google/callback', passport.authenticate('google', {
+          failureRedirect: '/signin'
+      }), users.authCallback);
+  <% } %>
+
   // All other routes to use Angular routing in app/scripts/app.js
   app.get('/partials/*', index.partials);
   app.get('/*',<% if(mongoPassportUser) { %> middleware.setUserCookie,<% } %> index.index);
