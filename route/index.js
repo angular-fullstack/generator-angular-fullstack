82b6d3adc752d04e389b22a6eb4efb3b912e345a
wmbutler <bill@butler.net>
2014-03-23 18:41:20 -0500

    Update to index.js
    Line 25 should include the path to view/partials and needs the .html file extension. I'm not clear on how the coffeescript piece should be implemented so please check my work regarding that since I don't use coffeescript.

--- route/index.js
+++ route/index.js
@@ -22,7 +22,7 @@ Generator.prototype.rewriteAppJs = function () {
     ),
     needle: '.otherwise',
     splicable: [
-      "  templateUrl: 'partials/" + this.name.toLowerCase() + "'" + (coffee ? "" : "," ),
+      "  templateUrl: 'views/partials/" + this.name.toLowerCase() + ".html" + "'" + (coffee ? "" : "," ),
       "  controller: '" + this.classedName + "Ctrl'"
     ]
   };
