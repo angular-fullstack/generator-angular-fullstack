443b9499b877f1ba76f27cc7dc8256c9e83b3af2
Tyler Henkel <tylerhenkel@hotmail.com>
2014-06-01 21:09:11 -0600

    Formatting

--- app/templates/server/auth(auth)/index.js
+++ app/templates/server/auth(auth)/index.js
@@ -6,16 +6,16 @@ var config = require('../config');
 var User = require('../api/user/user.model');
 
 // Passport Configuration
-require('./local/passport').setup(User, config);
-<% if (facebookAuth) { %>require('./facebook/passport').setup(User, config);<% } %>
-<% if (googleAuth) { %>require('./google/passport').setup(User, config);<% } %>
-<% if (twitterAuth) { %>require('./twitter/passport').setup(User, config);<% } %>
+require('./local/passport').setup(User, config);<% if (facebookAuth) { %>
+require('./facebook/passport').setup(User, config);<% } %><% if (googleAuth) { %>
+require('./google/passport').setup(User, config);<% } %><% if (twitterAuth) { %>
+require('./twitter/passport').setup(User, config);<% } %>
 
 var router = express.Router();
 
-router.use('/local', require('./local'));
-<% if (facebookAuth) { %>router.use('/facebook', require('./facebook'));<% } %>
-<% if (googleAuth) { %>router.use('/twitter', require('./twitter'));<% } %>
-<% if (twitterAuth) { %>router.use('/google', require('./google'));<% } %>
+router.use('/local', require('./local'));<% if (facebookAuth) { %>
+router.use('/facebook', require('./facebook'));<% } %><% if (googleAuth) { %>
+router.use('/twitter', require('./twitter'));<% } %><% if (twitterAuth) { %>
+router.use('/google', require('./google'));<% } %>
 
 module.exports = router;
