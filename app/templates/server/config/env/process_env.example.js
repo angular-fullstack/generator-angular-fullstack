ef6f9b2ef621b53caeb76250fe61d880df004d91
Tyler Henkel <tylerhenkel@hotmail.com>
2014-06-08 22:25:06 -0600

    added better env defaults

--- app/templates/server/config/env/process_env.example.js
+++ app/templates/server/config/env/process_env.example.js
@@ -3,11 +3,11 @@
 // Environment variables that Grunt will start the server with.
 // Add your settings to a local process_env.js, that will not be tracked by git
 module.exports = {
-  SESSION_SECRET: "angular-fullstack",
-  FACEBOOK_ID: "",
-  FACEBOOK_SECRET: "",
-  TWITTER_ID: "",
-  TWITTER_SECRET: "",
-  GOOGLE_ID: "",
-  GOOGLE_SECRET: ""
-}
+  SESSION_SECRET: "<%= _.slugify(_.humanize(appname)) + '-secret' %>",
+  FACEBOOK_ID: "FACEBOOK-APP-ID",
+  FACEBOOK_SECRET: "FACEBOOK-SECRET",
+  TWITTER_ID: "TWITTER-APP-ID",
+  TWITTER_SECRET: "TWITTER-SECRET",
+  GOOGLE_ID: "GOOGLE-APP-ID",
+  GOOGLE_SECRET: "GOOGLE-SECRET"
+};
