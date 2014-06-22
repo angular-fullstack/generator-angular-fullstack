fabf48842181d47586462edf682879ddc10709de
Tyler Henkel <tylerhenkel@hotmail.com>
2014-06-22 01:12:59 -0600

    fix relative app root in config

--- app/templates/server/config/environment/index.js
+++ app/templates/server/config/environment/index.js
@@ -16,7 +16,7 @@ var all = {
   env: process.env.NODE_ENV,
 
   // Root path of server
-  root: path.normalize(__dirname + '/../..'),
+  root: path.normalize(__dirname + '/../../..'),
 
   // Server port
   port: process.env.PORT || 9000,
