1246c98ef0a8ccf8e15168a74b3e2d99dd239cd2
Tyler Henkel <tylerhenkel@hotmail.com>
2014-06-21 20:34:32 -0600

    fix procfile not getting commited, and output more deployment information

--- heroku/index.js
+++ heroku/index.js
@@ -24,8 +24,7 @@ Generator.prototype.askForName = function askForName() {
 
   var prompts = [{
     name: 'deployedName',
-    message: 'Name to deploy as:',
-    default: this.appname
+    message: 'Name to deploy as (Leave blank for a random name):'
   }];
 
   this.prompt(prompts, function (props) {
@@ -54,10 +53,12 @@ Generator.prototype.gitInit = function gitInit() {
 
   this.log(chalk.bold('\nInitializing deployment repo'));
   this.mkdir('dist');
-  exec('git init', { cwd: 'dist' }, function (err, stdout, stderr) {
-    this.log(stdout);
+  var child = exec('git init', { cwd: 'dist' }, function (err, stdout, stderr) {
     done();
   }.bind(this));
+  child.stdout.on('data', function(data) {
+    console.log(data.toString());
+  });
 };
 
 Generator.prototype.herokuCreate = function herokuCreate() {
@@ -65,21 +66,31 @@ Generator.prototype.herokuCreate = function herokuCreate() {
   var done = this.async();
 
   this.log(chalk.bold('Creating heroku app and setting node environment'));
-  exec('heroku apps:create ' + this.deployedName + ' && heroku config:set NODE_ENV=production', { cwd: 'dist' }, function (err, stdout, stderr) {
+  var child = exec('heroku apps:create ' + this.deployedName + ' && heroku config:set NODE_ENV=production', { cwd: 'dist' }, function (err, stdout, stderr) {
     if (err) {
-      this.log.error(err);
       this.abort = true;
+      this.log.error(err);
     } else {
       this.log('stdout: ' + stdout);
     }
     done();
   }.bind(this));
+
+  child.stdout.on('data', function(data) {
+    var output = data.toString();
+    this.log(output);
+  }.bind(this));
+  child.stdin.write('\n\n');
 };
 
 Generator.prototype.copyProcfile = function copyProcfile() {
   if(this.abort) return;
+  var done = this.async();
   this.log(chalk.bold('Creating Procfile'));
-  this.template('Procfile', 'dist/Procfile');
+  this.copy('Procfile', 'dist/Procfile');
+  this.conflicter.resolve(function (err) {
+    done();
+  });
 };
 
 Generator.prototype.gruntBuild = function gruntBuild() {
@@ -87,12 +98,11 @@ Generator.prototype.gruntBuild = function gruntBuild() {
   var done = this.async();
 
   this.log(chalk.bold('\nBuilding dist folder, please wait...'));
-  exec('grunt build', function (err, stdout) {
-    this.log('stdout: ' + stdout);
-    if (err) {
-      this.log.error(err);
-    }
+  var child = exec('grunt build', function (err, stdout) {
     done();
+  }.bind(this));
+  child.stdout.on('data', function(data) {
+    this.log(data.toString());
   }.bind(this));
 };
 
@@ -101,7 +111,7 @@ Generator.prototype.gitCommit = function gitInit() {
   var done = this.async();
 
   this.log(chalk.bold('Adding files for initial commit'));
-  exec('git add -A && git commit -m "Initial commit"', { cwd: 'dist' }, function (err, stdout, stderr) {
+  var child = exec('git add -A && git commit -m "Initial commit"', { cwd: 'dist' }, function (err, stdout, stderr) {
     if (stdout.search('nothing to commit') >= 0) {
       this.log('Re-pushing the existing "dist" build...');
     } else if (err) {
@@ -111,6 +121,10 @@ Generator.prototype.gitCommit = function gitInit() {
     }
     done();
   }.bind(this));
+
+  child.stdout.on('data', function(data) {
+    this.log(data.toString());
+  }.bind(this));
 };
 
 Generator.prototype.gitForcePush = function gitForcePush() {
@@ -119,7 +133,7 @@ Generator.prototype.gitForcePush = function gitForcePush() {
 
   this.log(chalk.bold("\nUploading your initial application code.\n This may take "+chalk.cyan('several minutes')+" depending on your connection speed..."));
 
-  exec('git push -f heroku master', { cwd: 'dist' }, function (err, stdout, stderr) {
+  var child = exec('git push -f heroku master', { cwd: 'dist' }, function (err, stdout, stderr) {
     if (err) {
       this.log.error(err);
     } else {
@@ -127,8 +141,12 @@ Generator.prototype.gitForcePush = function gitForcePush() {
       this.log(chalk.cyan('If you\'re using mongoDB, be sure to add a database to your heroku app.\n\t' + chalk.bold('heroku addons:add mongohq') + '\n\n'));
       this.log(chalk.yellow('To deploy a new build\n\t' + chalk.bold('grunt build') +
                 '\nThen enter the dist folder to commit these updates:\n\t' + chalk.bold('cd dist && git commit -am "describe your changes here"')));
-      this.log(chalk.green('Finally, deploy your updated build to Heroku with\n\t' + chalk.bold('git push heroku master')));
+      this.log(chalk.green('Finally, deploy your updated build to Heroku with\n\t' + chalk.bold('git push -f heroku master')));
     }
     done();
+  }.bind(this));
+
+  child.stdout.on('data', function(data) {
+    this.log(data.toString());
   }.bind(this));
 };
