bdc9541803a31fff1ecc30b58299c2a8fb0c30d2
Tyler Henkel <tylerhenkel@hotmail.com>
2014-06-21 23:54:39 -0600

    add sass to travis and use phantomjs for default test runner

--- app/templates/karma.conf.js
+++ app/templates/karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
