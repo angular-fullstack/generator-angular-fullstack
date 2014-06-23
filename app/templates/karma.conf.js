aa7433357da4d06404f2afed2fe8de179689cf0a
Tyler Henkel <tylerhenkel@hotmail.com>
2014-06-22 23:23:35 -0600

    log mode back to info

--- app/templates/karma.conf.js
+++ app/templates/karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
 
 
     // enable / disable watching file and executing tests whenever any file changes
