8851bb1c89a9b3c9a43fc75e266bdc9a527aee74
Tyler Henkel <tylerhenkel@hotmail.com>
2014-06-23 00:10:23 -0600

    fix auth modules mapped wrong

--- app/templates/server/auth(auth)/index.js
+++ app/templates/server/auth(auth)/index.js
@@ -14,8 +14,8 @@ require('./twitter/passport').setup(User, config);<% } %>
 var router = express.Router();
 
 router.use('/local', require('./local'));<% if (filters.facebookAuth) { %>
-router.use('/facebook', require('./facebook'));<% } %><% if (filters.googleAuth) { %>
-router.use('/twitter', require('./twitter'));<% } %><% if (filters.twitterAuth) { %>
+router.use('/facebook', require('./facebook'));<% } %><% if (filters.twitterAuth) { %>
+router.use('/twitter', require('./twitter'));<% } %><% if (filters.googleAuth) { %>
 router.use('/google', require('./google'));<% } %>
 
 module.exports = router;
