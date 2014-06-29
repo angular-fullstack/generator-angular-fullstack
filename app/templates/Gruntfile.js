283dd1eb775812ed706ca252ceb000fcd82e8806
Tyler Henkel <tylerhenkel@hotmail.com>
2014-06-29 00:05:30 -0600

    fix module name not correct in ng-templates grunt task

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -321,7 +321,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       options: {
         // This should be the name of your apps angular module
-        module: require('./bower.json').name + 'App',
+        module: '<%= scriptAppName %>',
         htmlmin: {
           collapseBooleanAttributes: true,
           collapseWhitespace: true,
