18c6222c8528bd0eb1b041a8bc653009e3c51f9a
eike thies <eike.thies@fileee.com>
2014-09-19 20:12:59 +0200

    renamed password field for exclution

--- app/templates/server/api/user(auth)/user.controller.js
+++ app/templates/server/api/user(auth)/user.controller.js
@@ -31,7 +31,7 @@ function respondWith(res, statusCode){
  * restriction: 'admin'
  */
 exports.index = function(req, res) {
-  User.findAsync({}, '-salt -hashedPassword')
+  User.findAsync({}, '-salt -password')
     .then(function (users) {
       res.json(200, users);
     })
@@ -108,7 +108,7 @@ exports.changePassword = function(req, res, next) {
 exports.me = function(req, res, next) {
   var userId = req.user._id;
 
-  User.findOneAsync({ _id: userId }, '-salt -hashedPassword')
+  User.findOneAsync({ _id: userId }, '-salt -password')
     .then(function(user) { // don't ever give out the password or salt
       if (!user) { return res.json(401); }
       res.json(user);
