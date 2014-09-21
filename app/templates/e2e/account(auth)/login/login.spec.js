0fbea07d92e3e157ea013e5a77a2b4c51b636089
kingcody <me@codymize.com>
2014-09-21 05:09:45 -0400

    test(app): add protractor tests for account pages
    Changes:
    - add protractor tests for auth related pages
    - add `onPrepare` and `params` to `protractor.conf.js`

---
+++ app/templates/e2e/account(auth)/login/login.spec.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var config = protractor.getInstance().params;
+var UserModel = require(config.serverConfig.root + '/server/api/user/user.model');
+
+describe('Login View', function() {
+  var page;
+
+  var loadPage = function() {
+    browser.get('/login');
+    page = require('./login.po');
+  };
+
+  var testUser = {
+    name: 'Test User',
+    email: 'test@test.com',
+    password: 'test'
+  };
+
+  beforeEach(function(done) {
+    UserModel.removeAsync()
+      .then(function() {
+        return UserModel.createAsync(testUser);
+      })
+      .then(loadPage)
+      .finally(done);
+  });
+
+  it('should include login form with correct inputs and submit button', function() {
+    expect(page.form.email.getAttribute('type')).toBe('email');
+    expect(page.form.email.getAttribute('name')).toBe('email');
+    expect(page.form.password.getAttribute('type')).toBe('password');
+    expect(page.form.password.getAttribute('name')).toBe('password');
+    expect(page.form.submit.getAttribute('type')).toBe('submit');
+    expect(page.form.submit.getText()).toBe('Login');
+  });
+
+  describe('with local auth', function() {
+
+    it('should login a user and redirecting to "/"', function() {
+      page.login(testUser);
+
+      var navbar = require('../../components/navbar/navbar.po');
+
+      expect(browser.getLocationAbsUrl()).toBe(config.baseUrl + '/');
+      expect(navbar.navbarAccountGreeting.getText()).toBe('Hello ' + testUser.name);
+    });
+
+    it('should indicate login failures', function() {
+      page.login({
+        email: testUser.email,
+        password: 'badPassword'
+      });
+
+      expect(browser.getLocationAbsUrl()).toBe(config.baseUrl + '/login');
+
+      var helpBlock = page.form.element(by.css('.form-group.has-error .help-block.ng-binding'));
+      expect(helpBlock.getText()).toBe('This password is not correct.');
+    });
+
+  });
+});
