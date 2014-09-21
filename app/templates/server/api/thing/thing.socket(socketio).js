56134c0c20a131e5fd14bb3f6cf00cd67d7fc0c3
Tyler Henkel <tylerhenkel@hotmail.com>
2014-09-20 20:59:01 -0600

    style(server): missing semicolon

--- app/templates/server/api/thing/thing.socket(socketio).js
+++ app/templates/server/api/thing/thing.socket(socketio).js
@@ -13,7 +13,7 @@ exports.register = function(socket) {
   thing.schema.post('remove', function (doc) {
     onRemove(socket, doc);
   });
-}
+};
 
 function onSave(socket, doc, cb) {
   socket.emit('thing:save', doc);
--- endpoint/templates/name.socket(socketio).js
+++ endpoint/templates/name.socket(socketio).js
@@ -13,7 +13,7 @@ exports.register = function(socket) {
   <%= classedName %>.schema.post('remove', function (doc) {
     onRemove(socket, doc);
   });
-}
+};
 
 function onSave(socket, doc, cb) {
   socket.emit('<%= name %>:save', doc);
