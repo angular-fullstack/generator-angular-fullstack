4fda6fb282e77b3ae9a8f60860d7e952f089d5cc
Andrew Stuart <andrew.stuart2@gmail.com>
2014-09-26 00:00:59 -0700

    Update spec to use 401 if endpoint authenticated.

--- endpoint/templates/name.spec.js
+++ endpoint/templates/name.spec.js
@@ -9,7 +9,7 @@ describe('GET <%= route %>', function() {
   it('should respond with JSON array', function(done) {
     request(app)
       .get('<%= route %>')
-      .expect(200)
+      .expect(<% if(authenticated) { %>401<% } else { %>200<% } %>)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
