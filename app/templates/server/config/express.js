f29e907d4388e8a615b61077984c00c9f202820a
Tyler Henkel <tylerhenkel@hotmail.com>
2014-10-02 21:25:04 -0600

    fix appPath for serving index.html

--- app/templates/server/config/express.js
+++ app/templates/server/config/express.js
@@ -42,18 +42,18 @@ module.exports = function(app) {
     store: new mongoStore({ mongoose_connection: mongoose.connection })
   }));
 <% } %>
+  app.set('appPath', path.join(config.root, 'client'));
+
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'client', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', config.root + '/client');
+    app.use(express.static(app.get('appPath')));
     app.use(morgan('dev'));
   }
 
   if ('development' === env || 'test' === env) {
     app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
-    app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', 'client');
+    app.use(express.static(app.get('appPath')));
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
   }
