8f07c8f7deb05aec569c3ed0cc8f66c6a6f7f42b
Tyler Henkel <tylerhenkel@hotmail.com>
2014-10-02 21:20:15 -0600

    fix sendFile on routes

--- app/templates/server/config/express.js
+++ app/templates/server/config/express.js
@@ -45,7 +45,7 @@ module.exports = function(app) {
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', config.root + '/public');
+    app.set('appPath', path.join(config.root, 'public'));
     app.use(morgan('dev'));
   }
 
@@ -53,7 +53,7 @@ module.exports = function(app) {
     app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', 'client');
+    app.set('appPath', path.join(config.root, 'client'));
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
   }
