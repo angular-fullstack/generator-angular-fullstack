98bb27d149ce4797ca5fea465a67bd60b61fa234
KeriWarr <keri@warr.ca>
2014-11-19 14:47:33 -0500

    refactor: deprecate res.[send|json](status)

--- app/templates/server/auth(auth)/auth.service.js
+++ app/templates/server/auth(auth)/auth.service.js
@@ -27,7 +27,7 @@ function isAuthenticated() {
     .use(function(req, res, next) {
       User.findById(req.user._id, function (err, user) {
         if (err) return next(err);
-        if (!user) return res.send(401);
+        if (!user) return res.status(401).send('Unauthorized');
 
         req.user = user;
         next();
@@ -48,7 +48,7 @@ function hasRole(roleRequired) {
         next();
       }
       else {
-        res.send(403);
+        res.status(403).send('Forbidden');
       }
     });
 }
@@ -64,7 +64,7 @@ function signToken(id) {
  * Set token cookie directly for oAuth strategies
  */
 function setTokenCookie(req, res) {
-  if (!req.user) return res.json(404, { message: 'Something went wrong, please try again.'});
+  if (!req.user) return res.status(404).json({ message: 'Something went wrong, please try again.'});
   var token = signToken(req.user._id, req.user.role);
   res.cookie('token', JSON.stringify(token));
   res.redirect('/');
--- endpoint/templates/name.controller.js
+++ endpoint/templates/name.controller.js
@@ -8,7 +8,7 @@ exports.index = function(req, res) {<% if (!filters.mongoose) { %>
   res.json([]);<% } %><% if (filters.mongoose) { %>
   <%= classedName %>.find(function (err, <%= name %>s) {
     if(err) { return handleError(res, err); }
-    return res.json(200, <%= name %>s);
+    return res.status(200).json(<%= name %>s);
   });<% } %>
 };<% if (filters.mongoose) { %>
 
@@ -16,7 +16,7 @@ exports.index = function(req, res) {<% if (!filters.mongoose) { %>
 exports.show = function(req, res) {
   <%= classedName %>.findById(req.params.id, function (err, <%= name %>) {
     if(err) { return handleError(res, err); }
-    if(!<%= name %>) { return res.send(404); }
+    if(!<%= name %>) { return res.status(404).send('Not Found'); }
     return res.json(<%= name %>);
   });
 };
@@ -25,7 +25,7 @@ exports.show = function(req, res) {
 exports.create = function(req, res) {
   <%= classedName %>.create(req.body, function(err, <%= name %>) {
     if(err) { return handleError(res, err); }
-    return res.json(201, <%= name %>);
+    return res.status(201).json(<%= name %>);
   });
 };
 
@@ -34,11 +34,11 @@ exports.update = function(req, res) {
   if(req.body._id) { delete req.body._id; }
   <%= classedName %>.findById(req.params.id, function (err, <%= name %>) {
     if (err) { return handleError(res, err); }
-    if(!<%= name %>) { return res.send(404); }
+    if(!<%= name %>) { return res.status(404).send('Not Found'); }
     var updated = _.merge(<%= name %>, req.body);
     updated.save(function (err) {
       if (err) { return handleError(res, err); }
-      return res.json(200, <%= name %>);
+      return res.status(200).json(<%= name %>);
     });
   });
 };
@@ -47,14 +47,14 @@ exports.update = function(req, res) {
 exports.destroy = function(req, res) {
   <%= classedName %>.findById(req.params.id, function (err, <%= name %>) {
     if(err) { return handleError(res, err); }
-    if(!<%= name %>) { return res.send(404); }
+    if(!<%= name %>) { return res.status(404).send('Not Found'); }
     <%= name %>.remove(function(err) {
       if(err) { return handleError(res, err); }
-      return res.send(204);
+      return res.status(204).send('No Content');
     });
   });
 };
 
 function handleError(res, err) {
-  return res.send(500, err);
+  return res.status(500).send(err);
 }<% } %>
