bb4d92e3050c2ca08a571f32632dab50ac430b5b
Mark Chapman <mchapman@users.noreply.github.com>
2014-12-31 16:31:11 +0000

    fix(gen:build): Adds missing slash
    fix(gen:build): Adds missing slash
    
    Adds missing slash

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -137,7 +137,7 @@ module.exports = function (grunt) {
           '{.tmp,<%%= yeoman.client %>}/{app,components}/**/*.css',
           '{.tmp,<%%= yeoman.client %>}/{app,components}/**/*.html',
           '{.tmp,<%%= yeoman.client %>}/{app,components}/**/*.js',
-          '!{.tmp,<%%= yeoman.client %>}{app,components}/**/*.spec.js',
+          '!{.tmp,<%%= yeoman.client %>}/{app,components}/**/*.spec.js',
           '!{.tmp,<%%= yeoman.client %>}/{app,components}/**/*.mock.js',
           '<%%= yeoman.client %>/assets/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}'
         ],
