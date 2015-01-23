af69a682f60daf9842df388877f4876d4fae9178
Tyler Henkel <tylerhenkel@hotmail.com>
2015-01-23 00:10:58 -0700

    style(jscs): update jscs definition to use settings closer to the angular repo

--- app/templates/server/api/thing/thing.controller(noModels).js
+++ app/templates/server/api/thing/thing.controller(noModels).js
@@ -12,27 +12,27 @@
 // Get list of things
 exports.index = function(req, res) {
   res.json([{
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, ' +
-           'Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
+    name: 'Development Tools',
+    info: 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, ' +
+          'Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
   }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
+    name: 'Server and Client integration',
+    info: 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
   }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. ' +
-           'Automatic injection of scripts and styles into your index.html'
+    name: 'Smart Build System',
+    info: 'Build system ignores `spec` files, allowing you to keep tests alongside code. ' +
+          'Automatic injection of scripts and styles into your index.html'
   }, {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and ' +
-           'maximum scalability'
+    name: 'Modular Structure',
+    info: 'Best practice client and server structures allow for more code reusability and ' +
+          'maximum scalability'
   }, {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies ' +
-           'your scripts/css/images, and rewrites asset names for caching.'
+    name: 'Optimized Build',
+    info: 'Build process packs up your templates as a single JavaScript payload, minifies ' +
+          'your scripts/css/images, and rewrites asset names for caching.'
   }, {
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift ' +
-           'subgenerators'
+    name: 'Deployment Ready',
+    info: 'Easily deploy your app to Heroku or Openshift with the heroku and openshift ' +
+          'sub-generators'
   }]);
 };
--- app/templates/server/config/seed(models).js
+++ app/templates/server/config/seed(models).js
@@ -20,31 +20,31 @@ var Thing = sqldb.Thing;
   .then(function() {
     <% if (filters.mongooseModels) { %>Thing.create({<% }
        if (filters.sequelizeModels) { %>Thing.bulkCreate([{<% } %>
-      name : 'Development Tools',
-      info : 'Integration with popular tools such as Bower, Grunt, Karma, ' +
+      name: 'Development Tools',
+      info: 'Integration with popular tools such as Bower, Grunt, Karma, ' +
              'Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, ' +
              'Stylus, Sass, CoffeeScript, and Less.'
     }, {
-      name : 'Server and Client integration',
-      info : 'Built with a powerful and fun stack: MongoDB, Express, ' +
+      name: 'Server and Client integration',
+      info: 'Built with a powerful and fun stack: MongoDB, Express, ' +
              'AngularJS, and Node.'
     }, {
-      name : 'Smart Build System',
-      info : 'Build system ignores `spec` files, allowing you to keep ' +
+      name: 'Smart Build System',
+      info: 'Build system ignores `spec` files, allowing you to keep ' +
              'tests alongside code. Automatic injection of scripts and ' +
              'styles into your index.html'
     }, {
-      name : 'Modular Structure',
-      info : 'Best practice client and server structures allow for more ' +
+      name: 'Modular Structure',
+      info: 'Best practice client and server structures allow for more ' +
              'code reusability and maximum scalability'
     }, {
-      name : 'Optimized Build',
-      info : 'Build process packs up your templates as a single JavaScript ' +
+      name: 'Optimized Build',
+      info: 'Build process packs up your templates as a single JavaScript ' +
              'payload, minifies your scripts/css/images, and rewrites asset ' +
              'names for caching.'
     }, {
-      name : 'Deployment Ready',
-      info : 'Easily deploy your app to Heroku or Openshift with the heroku ' +
+      name: 'Deployment Ready',
+      info: 'Easily deploy your app to Heroku or Openshift with the heroku ' +
              'and openshift subgenerators'
     <% if (filters.mongooseModels) { %>});<% }
        if (filters.sequelizeModels) { %>}]);<% } %>
