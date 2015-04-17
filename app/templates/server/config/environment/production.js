b5a629e187969e88299cafb8e10326e30815847d
Kenneth Falck <kennu@iki.fi>
2015-04-17 12:58:15 +0300

    feat(gen) add Dockerfile and docker-compose.yml support
    Add support to run generated Angular-Fullstack applications as Docker containers by including
    Dockerfile, docker-compose.yml and .dockerignore. When the application is built with grunt
    build, these files are copied to the dist directory. The Docker container can then be built
    and started there with the "docker-compose up" command. When Mongoose is used, Docker Compose
    will also launch a MongoDB container, eliminating the need to install MongoDB on the local
    machine. Note that Docker and Docker Compose must be installed for Docker support to work.
    They are available on Linux, OS X and Windows.

--- app/templates/server/config/environment/production.js
+++ app/templates/server/config/environment/production.js
@@ -18,7 +18,11 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL +
-            process.env.OPENSHIFT_APP_NAME ||
+            process.env.OPENSHIFT_APP_NAME ||<% if (filters.docker) { %>
+            (process.env.MONGODB_PORT_27017_TCP_ADDR ? 'mongodb://' +
+              process.env.MONGODB_PORT_27017_TCP_ADDR + ':' +
+              process.env.MONGODB_PORT_27017_TCP_PORT +
+              '/<%= _.slugify(appname) %>' : null) ||<% } %>
             'mongodb://localhost/<%= _.slugify(appname) %>'
   }
 };
