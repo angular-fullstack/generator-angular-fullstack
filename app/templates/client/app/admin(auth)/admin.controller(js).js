366d03297e3cc76928a7fa7aaac0e209baad2221
Denis Ciccale <dciccale@gmail.com>
2015-06-02 11:28:46 +0200

    chore(admin): splice user without iteration

--- app/templates/client/app/admin(auth)/admin.controller(js).js
+++ app/templates/client/app/admin(auth)/admin.controller(js).js
@@ -8,10 +8,6 @@ angular.module('<%= scriptAppName %>')
 
     $scope.delete = function(user) {
       User.remove({ id: user._id });
-      angular.forEach($scope.users, function(u, i) {
-        if (u === user) {
-          $scope.users.splice(i, 1);
-        }
-      });
+      $scope.users.splice(this.$index, 1);
     };
   });
