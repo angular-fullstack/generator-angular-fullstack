d78e0f64f6f9e500b59430c8d9f8c29b4a7af314
Andrew Koroluk <koroluka@gmail.com>
2015-06-21 20:50:59 -0400

    refactor(gulp): Use arrow functions in a bunch of places
    [ci skip]

--- app/templates/gulpfile.babel.js
+++ app/templates/gulpfile.babel.js
@@ -68,26 +68,24 @@ function checkAppReady(cb) {
         host: 'localhost',
         port: config.port,
     };
-    http.get(options, function() {
-        cb(true);
-    }).on('error', function() {
-        cb(false);
-    });
+    http
+        .get(options, () => cb(true))
+        .on('error', () => cb(false));
 }
 
 // Call page until first success
 function whenServerReady(cb) {
     var serverReady = false;
-    var appReadyInterval = setInterval(function() {
-        checkAppReady(function(ready) {
+    var appReadyInterval = setInterval(() =>
+        checkAppReady((ready) => {
             if(!ready || serverReady) {
                 return;
             }
             clearInterval(appReadyInterval);
             serverReady = true;
             cb();
-        });
-    }, 100);
+        }),
+        100);
 }
 
 ////////////////////////
@@ -113,41 +111,34 @@ var styles = lazypipe()<% if(filters.stylus) { %>
 // Tasks //
 ///////////
 
-gulp.task('styles', function () {
-    return styles();
-});<% if(filters.coffee) { %>
+gulp.task('styles', () => styles());<% if(filters.coffee) { %>
 
-gulp.task('coffee', function() {
-    return gulp.src(paths.client.scripts)
+gulp.task('coffee', () =>
+    gulp.src(paths.client.scripts)
         .pipe(lintScripts())
         .pipe(plugins.coffee({bare: true}).on('error', plugins.util.log))
         .pipe(gulp.dest('.tmp/scripts'));
-});<% } %>
+);<% } %>
 
-gulp.task('lint:scripts', function () {
-    var scripts = paths.client.scripts.concat(paths.server.scripts);
-    return gulp.src(scripts).pipe(lintScripts());
-});
+gulp.task('lint:scripts', () => gulp.src(paths.client.scripts.concat(paths.server.scripts)).pipe(lintScripts()));
 
-gulp.task('clean:tmp', function () {
-    return gulp.src('.tmp', {read: false}).pipe(plugins.clean());
-});
+gulp.task('clean:tmp', () => gulp.src('.tmp', {read: false}).pipe(plugins.clean()));
 
-gulp.task('start:client', [<% if(filters.coffee) { %>'coffee', <% } %>'styles'], function (callback) {
-    whenServerReady(function () {
+gulp.task('start:client', [<% if(filters.coffee) { %>'coffee', <% } %>'styles'], (callback) => {
+    whenServerReady(() => {
         open('http://localhost:' + config.port);
         callback();
     });
 });
 
-gulp.task('start:server', function () {
+gulp.task('start:server', () => {
     process.env.NODE_ENV = process.env.NODE_ENV || 'development';
     config = require('./server/config/environment');
     nodemon('-w lib server/app.js')
         .on('log', onServerLog);
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
     var testFiles = paths.client.test.concat(paths.server.test);
 
     plugins.watch(paths.client.styles)
@@ -173,23 +164,24 @@ gulp.task('watch', function () {
     gulp.watch('bower.json', ['bower']);
 });
 
-gulp.task('serve', function (callback) {
+gulp.task('serve', (callback) =>
     runSequence('clean:tmp',
         ['lint:scripts'],
         'bower',
         ['start:server', 'start:client'],
-        'watch', callback);
-});
+        'watch',
+        callback);
+);
 
-gulp.task('test:server', function () {
+gulp.task('test:server', () => {
     process.env.NODE_ENV = 'test';
     return gulp.src(paths.server.test)
         .pipe(plugins.mocha({reporter: 'spec'}));
 });
 
-gulp.task('test:client', function () {
+gulp.task('test:client', () => {
     var testFiles = paths.client.testRequire.concat(paths.client.test)
-    gulp.src(testFiles)
+    return gulp.src(testFiles)
         .pipe(plugins.karma({
             configFile: paths.karma,
             action: 'watch'
@@ -197,29 +189,27 @@ gulp.task('test:client', function () {
 });
 
 // inject bower components
-gulp.task('bower', function () {
-    return gulp.src(paths.views.main)
+gulp.task('bower', () =>
+    gulp.src(paths.views.main)
         .pipe(wiredep({
             exclude: [/bootstrap-sass-official/, /bootstrap.js/, '/json3/', '/es5-shim/', /bootstrap.css/, /font-awesome.css/ ]
         }))
-    .pipe(gulp.dest('client/'));
-});
+        .pipe(gulp.dest('client/'));
+);
 
 ///////////
 // Build //
 ///////////
 
-gulp.task('build', function (callback) {
+gulp.task('build', (callback) =>
     runSequence('clean:dist',
         ['images', 'copy:extras', 'copy:fonts', 'copy:server', 'client:build'],
         callback);
-});
+);
 
-gulp.task('clean:dist', function () {
-    return gulp.src('dist', {read: false}).pipe(plugins.clean());
-});
+gulp.task('clean:dist', () => gulp.src('dist', {read: false}).pipe(plugins.clean()));
 
-gulp.task('client:build', ['html'], function () {
+gulp.task('client:build', ['html'], () => {
     var jsFilter = plugins.filter('**/*.js');
     var cssFilter = plugins.filter('**/*.css');<% if(filters.jade) { %>
     var assets = plugins.filter('**/*.{js,css}');<% } %>
@@ -242,35 +232,36 @@ gulp.task('client:build', ['html'], function () {
         .pipe(gulp.dest(paths.dist + '/public'));
 });
 
-gulp.task('html', function () {
-    return gulp.src(yeoman.app + '/views/**/*')
+gulp.task('html', () =>
+    gulp.src(yeoman.app + '/views/**/*')
         .pipe(gulp.dest(paths.dist + '/public/views'));
-});
+);
 
-gulp.task('images', function () {
-    return gulp.src(yeoman.app + '/images/**/*')
+gulp.task('images', () =>
+    gulp.src(yeoman.app + '/images/**/*')
         .pipe(plugins.cache(plugins.imagemin({
             optimizationLevel: 5,
             progressive: true,
             interlaced: true
         })))
         .pipe(gulp.dest(paths.dist + '/public/images'));
-});
+);
 
-gulp.task('copy:extras', function () {
-    return gulp.src(yeoman.app + '/*.*', { dot: true })
+gulp.task('copy:extras', () =>
+    gulp.src(yeoman.app + '/*.*', { dot: true })
         .pipe(gulp.dest(paths.dist + '/public'));
-});
+);
 
-gulp.task('copy:fonts', function () {
-    return gulp.src(yeoman.app + '/fonts/**/*')
+gulp.task('copy:fonts', () =>
+    gulp.src(yeoman.app + '/fonts/**/*')
         .pipe(gulp.dest(paths.dist + '/fonts'));
-});
+);
 
-gulp.task('copy:server', function(){
-    return gulp.src([
+gulp.task('copy:server', () =>
+    gulp.src([
         'package.json',
         'server.js',
         'lib/**/*'
-    ], {cwdbase: true}).pipe(gulp.dest(paths.dist));
-});
+    ], {cwdbase: true})
+        .pipe(gulp.dest(paths.dist));
+);
