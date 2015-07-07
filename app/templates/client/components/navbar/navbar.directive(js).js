24171aa4962b460770876bf6232f6b82a27e9925
kingcody <me@codymize.com>
2015-07-07 18:30:59 -0400

    feat(app): implement navbar as directive

---
+++ app/templates/client/components/navbar/navbar.directive(js).js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('<%= scriptAppName %>')
+  .directive('navbar', function () {
+    return {
+      templateUrl: 'components/navbar/navbar.html',
+      restrict: 'E',
+      controller: 'NavbarCtrl'
+    };
+  });
