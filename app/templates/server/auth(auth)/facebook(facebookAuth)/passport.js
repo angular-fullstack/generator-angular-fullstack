6b73bf8dd47680418fb44a86f6f194503ae8354b
Adam Keenan <adam.g.keenan@gmail.com>
2015-07-14 01:44:08 -0500

    Facebook removed most default info. Must now explicitly ask for email

--- app/templates/server/auth(auth)/facebook(facebookAuth)/passport.js
+++ app/templates/server/auth(auth)/facebook(facebookAuth)/passport.js
@@ -5,7 +5,11 @@ exports.setup = function (User, config) {
   passport.use(new FacebookStrategy({
       clientID: config.facebook.clientID,
       clientSecret: config.facebook.clientSecret,
-      callbackURL: config.facebook.callbackURL
+      callbackURL: config.facebook.callbackURL,
+      profileFields: [
+        'displayName',
+        'emails'
+      ]
     },
     function(accessToken, refreshToken, profile, done) {
       User.findOne({
