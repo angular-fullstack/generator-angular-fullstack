a4ba9647452bfbee2a38b905d7bebfa515826055
kingcody <me@codymize.com>
2015-09-04 13:10:10 -0400

    fix(gen): allow generator to be transpiled as a node_module
    closes #1244

--- endpoint/index.js
+++ endpoint/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 // Register the Babel require hook
-require('babel-core/register');
+require('babel-core/register')({
+  only: /generator-angular-fullstack\/(?!node_modules)/
+});
 
 // Export the generator
 exports = module.exports = require('./generator');
