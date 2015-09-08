72e046a4a3af1776f7c0375a0c1d2944518da8f7
Cody Mize <me@codymize.com>
2015-09-08 00:37:06 -0400

    fix(app): load babel if development or test env

--- app/templates/server/index.js
+++ app/templates/server/index.js
@@ -3,7 +3,7 @@
 // Set default node environment to development
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (env === 'production' || env === 'test') {
+if (env === 'development' || env === 'test') {
   // Register the Babel require hook
   require('babel-core/register');
 }
