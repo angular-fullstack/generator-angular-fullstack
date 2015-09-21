a8bd8a678d370035277309551e11e0870fa49b1e
kingcody <me@codymize.com>
2015-09-20 21:30:39 -0400

    fix(server:config): template db configs and add sequelize prod conf

--- app/templates/server/config/environment/production.js
+++ app/templates/server/config/environment/production.js
@@ -4,21 +4,33 @@
 // =================================
 module.exports = {
   // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
+  ip:     process.env.OPENSHIFT_NODEJS_IP ||
+          process.env.IP ||
+          undefined,
 
   // Server port
-  port:     process.env.OPENSHIFT_NODEJS_PORT ||
-            process.env.PORT ||
-            8080,
+  port:   process.env.OPENSHIFT_NODEJS_PORT ||
+          process.env.PORT ||
+          8080<% if (filters.mongoose) { %>,
 
   // MongoDB connection options
   mongo: {
-    uri:    process.env.MONGOLAB_URI ||
-            process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL +
-            process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/<%= lodash.slugify(appname) %>'
-  }
+    uri:  process.env.MONGOLAB_URI ||
+          process.env.MONGOHQ_URL ||
+          process.env.OPENSHIFT_MONGODB_DB_URL +
+          process.env.OPENSHIFT_APP_NAME ||
+          'mongodb://localhost/<%= lodash.slugify(appname) %>'
+  }<% } if (filters.sequelize) { %>,
+
+  sequelize: {
+    uri:  process.env.SEQUELIZE_URI ||
+          'sqlite://',
+    options: {
+      logging: false,
+      storage: 'dist.sqlite',
+      define: {
+        timestamps: false
+      }
+    }
+  }<% } %>
 };
