3229acdf54ddf3a0bfe7dc8a4ee654b3d4cdd84d
Andrew Koroluk <koroluka@gmail.com>
2015-12-05 04:33:59 -0500

    fix(client:auth): remove decorator logic
    it seems like it wasn't working, closes #1455

--- app/templates/client/components/auth(auth)/router.decorator.js
+++ app/templates/client/components/auth(auth)/router.decorator.js
@@ -2,40 +2,26 @@
 
 (function() {
 
-function routerDecorator(<%= filters.uirouter ? '$stateProvider' : '$provide' %>) {
-  var authDecorator = function(<%= filters.uirouter ? 'state' : 'route' %>) {
-    var auth = <%= filters.uirouter ? 'state' : 'route' %>.authenticate;
-    if (auth) {
-      <%= filters.uirouter ? 'state' : 'route' %>.resolve = <%= filters.uirouter ? 'state' : 'route' %>.resolve || {};
-      <%= filters.uirouter ? 'state' : 'route' %>.resolve.user = function(<%= filters.uirouter ? '$state' : '$location' %>, $q, Auth) {
-        return Auth.getCurrentUser(true)
-          .then(function(user) {
-            if ((typeof auth !== 'string' && user._id) ||
-              (typeof auth === 'string' && Auth.hasRole(auth))) {
-              return user;
-            }<% if (filters.ngroute) { %>
-            $location.path((user._id) ? '/' : '/login');<% } if (filters.uirouter) { %>
-            $state.go((user._id) ? 'main' : 'login');<% } %>
-            return $q.reject('not authorized');
-          });
-      };
-    }
-  };<% if (filters.ngroute) { %>
-
-  $provide.decorator('$route', function($delegate) {
-    for (var r in $delegate.routes) {
-      authDecorator($delegate.routes[r]);
-    }
-    return $delegate;
-  });<% } if (filters.uirouter) { %>
-
-  $stateProvider.decorator('authenticate', function(state) {
-    authDecorator(state);
-    return state.authenticate;
-  });<% } %>
-}
-
 angular.module('<%= scriptAppName %>.auth')
-  .config(routerDecorator);
+  .run(function($rootScope<% if (filters.ngroute) { %>, $location<% } if (filters.uirouter) { %>, $state<% } %>, Auth) {    
+    // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
+    $rootScope.$on(<% if (filters.ngroute) { %>'$routeChangeStart'<% } %><% if (filters.uirouter) { %>'$stateChangeStart'<% } %>, function(event, next) {    
+      if(!next.authenticate) {
+        return;
+      }
+
+      let query = typeof next.authenticate === 'string' ? Auth.hasRole : Auth.isLoggedIn;
+
+      query(1,2).then(good => {
+        if(!good) {
+          event.preventDefault();
+          Auth.isLoggedIn().then(is => {<% if (filters.ngroute) { %>
+            $location.path(is ? '/' : '/login');<% } if (filters.uirouter) { %>
+            $state.go(is ? 'main' : 'login');<% } %>
+          });
+        }
+      });
+    });    
+  });
 
 })();
