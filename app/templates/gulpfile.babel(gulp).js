4a5e02b93a3a39c8a485250e96ff9bf4508e5234
David Mohr <david.mohr@ardexa.com>
2015-12-17 07:55:48 +1100

    refactor(gulp): migrate mocha settings to lazypipe
    lift mocha settings to a lazypipe for reuse and update test globs to
    separate unit and integration server tests

--- app/templates/gulpfile.babel(gulp).js
+++ app/templates/gulpfile.babel(gulp).js
@@ -38,13 +38,12 @@ const paths = {
         bower: `${clientPath}/bower_components/`
     },
     server: {
-        scripts: [`${serverPath}/**/*.<%= scriptExt %>`],
+        scripts: [`${serverPath}/**/!(*.spec|*.integration).<%= scriptExt %>`],
         json: [`${serverPath}/**/*.json`],
-        test: [
-            `${serverPath}/**/*.spec.js`,
-            `${serverPath}/**/*.mock.js`,
-            `${serverPath}/**/*.integration.js`
-        ]
+        test: {
+          integration: `${serverPath}/**/*.integration.js`,
+          unit: `${serverPath}/**/*.spec.js`
+        }
     },
     karma: 'karma.conf.js',
     dist: 'dist'
@@ -318,7 +317,7 @@ gulp.task('start:server', () => {
 });
 
 gulp.task('watch', () => {
-    var testFiles = _.union(paths.client.test, paths.server.test);
+    var testFiles = _.union(paths.client.test, paths.server.test.unit, paths.server.test.integration);
 
     plugins.livereload.listen();
 
@@ -383,7 +382,7 @@ gulp.task('test:server', cb => {
 });
 
 gulp.task('mocha:unit', () => {
-    return gulp.src(paths.server.test)
+    return gulp.src(paths.server.test.unit)
         .pipe(mocha());
 });
 
