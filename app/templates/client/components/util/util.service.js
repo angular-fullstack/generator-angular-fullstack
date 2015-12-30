396cb5bba710f90d6e558dde50331dc49bf1b61c
Peter Fu <peter.fu@activecollabmod.com>
2015-12-30 10:51:43 +0800

    fix(client:util.urlParse): special treatment for IE

--- app/templates/client/components/util/util.service.js
+++ app/templates/client/components/util/util.service.js
@@ -26,6 +26,15 @@ function UtilService($window) {
     urlParse(url) {
       var a = document.createElement('a');
       a.href = url;
+
+      // IE doesn't populate all link properties when setting .href with a relative URL,
+      // however .href will return an absolute URL which then can be used on itself
+      // to populate these additional fields.
+      // http://stackoverflow.com/a/13405933
+      if (a.host === '') {
+        a.href = a.href;
+      }
+
       return a;
     },
 
