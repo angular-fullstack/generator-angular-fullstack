93dec1248def09821d7a46567b450dc1c5331bdb
David Mohr <david.mohr@ardexa.com>
2016-01-06 18:57:27 +1100

    fix(gulp:mocha): have tests clean up once complete
    mocha unit and integration tests will hang unless express (and
    optionally mongoose) are closed once the tests are complete

--- app/templates/server/app.js
+++ app/templates/server/app.js
@@ -34,7 +34,7 @@ require('./routes')(app);
 
 // Start server
 function startServer() {
-  server.listen(config.port, config.ip, function() {
+  app.angularFullstack = server.listen(config.port, config.ip, function() {
     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
   });
 }
