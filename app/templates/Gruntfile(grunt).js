885438f4f701ce926db68c7cdb4eac2c5ad01e00
Andrew Koroluk <koroluka@gmail.com>
2016-01-28 15:05:39 -0500

    fix(grunt:babel): don't try to transpile JSON files
    closes #1561

--- app/templates/Gruntfile(grunt).js
+++ app/templates/Gruntfile(grunt).js
@@ -636,7 +636,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%%= yeoman.server %>',
-          src: ['**/*.{js,json}'],
+          src: ['**/*.js'],
           dest: '<%%= yeoman.dist %>/<%%= yeoman.server %>'
         }]
       }
