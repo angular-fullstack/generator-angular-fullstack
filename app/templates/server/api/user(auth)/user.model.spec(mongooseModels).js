b29e9ef57a40cc012c026fe1661b6ffebc30c8ca
Carlos Ramos <carragom@gmail.com>
2016-02-09 10:20:02 -0600

    refactor(model:user): exclude password info by default
    based on PR #1049 from @Awk34 commit (5fecaba)

--- app/templates/server/api/user(auth)/user.model.spec(mongooseModels).js
+++ app/templates/server/api/user(auth)/user.model.spec(mongooseModels).js
@@ -52,6 +52,13 @@ describe('User Model', function() {
       return user.saveAsync();
     });
 
+    it('should exclude salt and hashedPassword by default', function() {
+      User.find({name: user.name}, function(err, _user) {
+        _user.should.not.have.property('salt');
+        _user.should.not.have.property('hashedPassword');
+      });
+    });
+
     it('should authenticate user if valid', function() {
       <%= expect() %>user.authenticate('password')<%= to() %>.be.true;
     });
