6899dd67c03ee5be173f790a0c82ff2e4c7a8b27
Marco Chávez <marcochavezf@hotmail.com>
2016-02-27 18:08:31 -0600

    chore(main): use component in templates/client/app/main

--- app/templates/client/app/main/main.controller.spec.js
+++ app/templates/client/app/main/main.controller.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Controller: MainController', function() {
+describe('Component: mainComponent', function() {
 
   // load the controller's module
   beforeEach(module('<%= scriptAppName %>'));<% if (filters.uirouter) {%>
@@ -8,26 +8,35 @@ describe('Controller: MainController', function() {
   beforeEach(module('socketMock'));<% } %>
 
   var scope;
-  var MainController;<% if (filters.uirouter) {%>
+  var mainComponent;<% if (filters.uirouter) {%>
   var state;<% } %>
   var $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function(_$httpBackend_, $controller, $rootScope<% if (filters.uirouter) {%>, $state<% } %>) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+  beforeEach(inject(function(
+    _$httpBackend_,
+    $http,
+    $componentController,
+    $rootScope<% if (filters.uirouter) {%>,
+    $state<% } %><% if (filters.socketio) {%>,
+    socket<% } %>) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('/api/things')
+        .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
 
-    scope = $rootScope.$new();<% if (filters.uirouter) {%>
-    state = $state;<% } %>
-    MainController = $controller('MainController', {
-      $scope: scope
-    });
+      scope = $rootScope.$new();<% if (filters.uirouter) {%>
+      state = $state;<% } %>
+      mainComponent = $componentController('main', {
+        $http: $http,
+        $scope: scope<% if (filters.socketio) {%>,
+        socket: socket<% } %>
+      });
   }));
 
   it('should attach a list of things to the controller', function() {
+    mainComponent.$onInit();
     $httpBackend.flush();<% if (filters.jasmine) { %>
-    expect(MainController.awesomeThings.length).toBe(4);<% } if (filters.mocha) { %>
-    <%= expect() %>MainController.awesomeThings.length<%= to() %>.equal(4);<% } %>
+    expect(mainComponent.awesomeThings.length).toBe(4);<% } if (filters.mocha) { %>
+    <%= expect() %>mainComponent.awesomeThings.length<%= to() %>.equal(4);<% } %>
   });
 });
--- app/templates/client/app/main/main.js
+++ app/templates/client/app/main/main.js
@@ -4,16 +4,12 @@ angular.module('<%= scriptAppName %>')
   <% if (filters.ngroute) { %>.config(function($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
+        template: '<main></main>'
       });
   });<% } %><% if (filters.uirouter) { %>.config(function($stateProvider) {
     $stateProvider
       .state('main', {
         url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
+        template: '<main></main>'
       });
   });<% } %>
