522853670e8e635d67ad35fd8154e2a7bd9b99b3
Andrew Koroluk <koroluka@gmail.com>
2016-03-09 20:12:05 -0500

    fix(grunt:filerev): don't rev assets/fonts/
    [skip ci]

--- app/templates/Gruntfile(grunt).js
+++ app/templates/Gruntfile(grunt).js
@@ -284,8 +284,7 @@ module.exports = function (grunt) {
       dist: {
         src: [
           '<%%= yeoman.dist %>/<%%= yeoman.client %>/!(bower_components){,*/}*.{js,css}',
-          '<%%= yeoman.dist %>/<%%= yeoman.client %>/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          '<%%= yeoman.dist %>/<%%= yeoman.client %>/assets/fonts/*'
+          '<%%= yeoman.dist %>/<%%= yeoman.client %>/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
       }
     },
