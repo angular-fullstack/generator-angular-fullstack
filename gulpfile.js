94d69dae395911fd1a0a6442257a89182081306f
Andrew Koroluk <koroluka@gmail.com>
2016-04-23 22:39:32 -0400

    feat(gen:gulp): port updateFixtures to Gulp (hot damn is it faster :fire:)
    [skip ci]

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 'use strict';
+var fs = require('fs');
+var path = require('path');
+var Promise = require('bluebird');
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var del = require('del');
@@ -30,4 +33,48 @@ gulp.task('build', cb => {
         'copy',
         cb
     );
+});
+
+var processJson = function(src, dest, opt) {
+    return new Promise((resolve, reject) => {
+        // read file, strip all ejs conditionals, and parse as json
+        fs.readFile(path.resolve(src), 'utf8', (err, data) => {
+            if(err) return reject(err);
+
+            var json = JSON.parse(data.replace(/<%(.*)%>/g, ''));
+
+            // set properties
+            json.name = opt.appName;
+            json.version = opt.genVer;
+            json.private = opt.private;
+
+            // stringify json and write it to the destination
+            fs.writeFile(path.resolve(dest), JSON.stringify(json, null, 2), err => {
+                if(err) reject(err);
+                else resolve();
+            });
+        });
+    });
+};
+
+function updateFixtures(target) {
+    const deps = target === 'deps';
+    const genVer = require('./package.json').version;
+    const dest = __dirname + (deps ? '/angular-fullstack-deps/' : '/test/fixtures/');
+    const appName = deps ? 'angular-fullstack-deps' : 'tempApp';
+
+    return Promise.all([
+        processJson('templates/app/_package.json', dest + 'package.json', {appName, genVer, private: !!deps}),
+        processJson('templates/app/_bower.json', dest + 'bower.json', {appName, genVer, private: !!deps})
+    ]);
+}
+
+gulp.task('updateFixtures', cb => {
+    return runSequence(['updateFixtures:test', 'updateFixtures:deps'], cb);
+});
+gulp.task('updateFixtures:test', () => {
+    return updateFixtures('test');
+});
+gulp.task('updateFixtures:deps', () => {
+    return updateFixtures('deps');
 });
