4b4db99d73985bba4db20e7664b12fafe09ccaf1
Andrew Koroluk <koroluka@gmail.com>
2016-05-01 03:35:31 -0400

    fix(e2e:main): fix yeoman.png regex
    allow for revved image name, fixes e2e tests on a prod environment

--- templates/app/e2e/main/main.spec(jasmine).js
+++ templates/app/e2e/main/main.spec(jasmine).js
@@ -12,7 +12,7 @@ describe('Main View', function() {
 
   it('should include jumbotron with correct data', function() {
     expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/yeoman.png$/);
+    expect(page.imgEl.getAttribute('src')).toMatch(/yeoman(\.[a-zA-Z0-9]*)?\.png$/);
     expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
   });
 });
