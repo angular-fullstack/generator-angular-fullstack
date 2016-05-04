5f8805d4f1fce32cf83184b44d0ce61fe692e48c
thegitfather <baranowski@joinpoint.org>
2016-05-04 13:41:47 +0200

    fix(server:oauth): fix mongoose validation when re-login using twitter oauth
    Problem occurs after second login attempt using passport-twitter.
    Error message: "ValidationError: The specified email address is already in use."

--- app/templates/server/api/user(auth)/user.model(mongooseModels).js
+++ app/templates/server/api/user(auth)/user.model(mongooseModels).js
@@ -95,6 +95,9 @@ UserSchema
   .path('email')
   .validate(function(value, respond) {
     var self = this;
+    if (authTypes.indexOf(this.provider) !== -1) {
+      return respond(true);
+    }
     return this.constructor.findOne({ email: value }).exec()
       .then(function(user) {
         if (user) {
