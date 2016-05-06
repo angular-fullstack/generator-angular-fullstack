ae313df8ae0afd90eaabcb3035ac2eb999ef49c6
Andrew Koroluk <koroluka@gmail.com>
2016-05-05 23:22:53 -0600

    fix(server): MONGOLAB_URI -> MONGODB_URI
    fixes #1838
    
    [skip ci]

--- templates/app/server/config/environment/production.js
+++ templates/app/server/config/environment/production.js
@@ -15,7 +15,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri:  process.env.MONGOLAB_URI ||
+    uri:  process.env.MONGODB_URI ||
           process.env.MONGOHQ_URL ||
           process.env.OPENSHIFT_MONGODB_DB_URL +
           process.env.OPENSHIFT_APP_NAME ||
