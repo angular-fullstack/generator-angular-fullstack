881d170dbea614b09f67047d62e24a769b5105d7
Andrew Koroluk <koroluka@gmail.com>
2016-06-15 22:18:30 -0400

    fix(test:client): fix client tests for TS

--- templates/app/spec.js
+++ templates/app/spec.js
@@ -10,13 +10,5 @@ require('./client/components/ui-router/ui-router.mock');<% } %>
 <%_ if(filters.socketio) { _%>
 require('./client/components/socket/socket.mock');<% } %>
 
-testsContext = require.context('./client', true, /\.spec\.js$/);
-// var keys = testsContext.keys();
-// console.log(keys);
-// console.log(keys[0]);
-// testsContext(keys[0]);
-// console.log(keys[1]);
-// testsContext(keys[1]);
-// console.log(keys[2]);
-// testsContext(keys[2]);
+testsContext = require.context('./client', true, /\.spec\.<%= scriptExt %>$/);
 testsContext.keys().forEach(testsContext);
