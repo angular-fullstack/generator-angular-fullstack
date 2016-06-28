427cbaff143260c02076ca82fd70bcfc857a655d
Andrew Koroluk <koroluka@gmail.com>
2016-06-28 11:18:58 -0400

    refactor(client:auth.service): clean up async methods

--- templates/app/client/components/auth(auth)/auth.service.js
+++ templates/app/client/components/auth(auth)/auth.service.js
@@ -6,8 +6,16 @@ export function AuthService($location, $http, $cookies, $q, appConfig, Util, Use
   var safeCb = Util.safeCb;
   var currentUser = {};
   var userRoles = appConfig.userRoles || [];
+  /**
+   * Check if userRole is >= role
+   * @param {String} userRole - role of current user
+   * @param {String} role - role to check against
+   */
+  var hasRole = function(userRole, role) {
+    return userRoles.indexOf(userRole) >= userRoles.indexOf(role);
+  };
 
-  if ($cookies.get('token') && $location.path() !== '/logout') {
+  if($cookies.get('token') && $location.path() !== '/logout') {
     currentUser = User.get();
   }
 
@@ -90,18 +98,15 @@ export function AuthService($location, $http, $cookies, $q, appConfig, Util, Use
 
     /**
      * Gets all available info on a user
-     *   (synchronous|asynchronous)
      *
-     * @param  {Function|*} callback - optional, funciton(user)
-     * @return {Object|Promise}
+     * @param  {Function} [callback] - funciton(user)
+     * @return {Promise}
      */
     getCurrentUser(callback) {
-      if (arguments.length === 0) {
-        return currentUser;
-      }
+      var value = currentUser.hasOwnProperty('$promise')
+        ? currentUser.$promise
+        : currentUser;
 
-      var value = (currentUser.hasOwnProperty('$promise')) ?
-        currentUser.$promise : currentUser;
       return $q.when(value)
         .then(user => {
           safeCb(callback)(user);
@@ -123,16 +128,11 @@ export function AuthService($location, $http, $cookies, $q, appConfig, Util, Use
 
     /**
      * Check if a user is logged in
-     *   (synchronous|asynchronous)
      *
-     * @param  {Function|*} callback - optional, function(is)
+     * @param  {Function} [callback] - function(is)
      * @return {Bool|Promise}
      */
     isLoggedIn(callback) {
-      if (arguments.length === 0) {
-        return currentUser.hasOwnProperty('role');
-      }
-
       return Auth.getCurrentUser(null)
         .then(user => {
           var is = user.hasOwnProperty('role');
@@ -152,25 +152,18 @@ export function AuthService($location, $http, $cookies, $q, appConfig, Util, Use
 
      /**
       * Check if a user has a specified role or higher
-      *   (synchronous|asynchronous)
       *
       * @param  {String}     role     - the role to check against
-      * @param  {Function|*} callback - optional, function(has)
+      * @param  {Function} [callback] - function(has)
       * @return {Bool|Promise}
       */
     hasRole(role, callback) {
-      var hasRole = function(r, h) {
-        return userRoles.indexOf(r) >= userRoles.indexOf(h);
-      };
-
-      if (arguments.length < 2) {
-        return hasRole(currentUser.role, role);
-      }
-
       return Auth.getCurrentUser(null)
         .then(user => {
-          var has = (user.hasOwnProperty('role')) ?
-            hasRole(user.role, role) : false;
+          var has = user.hasOwnProperty('role')
+            ? hasRole(user.role, role)
+            : false;
+
           safeCb(callback)(has);
           return has;
         });
@@ -183,10 +176,6 @@ export function AuthService($location, $http, $cookies, $q, appConfig, Util, Use
       * @return {Bool}
       */
     hasRoleSync(role) {
-      var hasRole = function(r, h) {
-        return userRoles.indexOf(r) >= userRoles.indexOf(h);
-      };
-
       return hasRole(currentUser.role, role);
     },
 
