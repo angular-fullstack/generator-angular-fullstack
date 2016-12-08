b09cfe514d30d1a9fb06a4302f2629f48ac478b1
Andrew Koroluk <koroluka@gmail.com>
2016-12-07 22:38:28 -0500

    fix(gen:factory): fix factory subgen location
    fixes #2381

--- src/generators/factory/index.js
+++ src/generators/factory/index.js
@@ -1,9 +1,9 @@
 'use strict';
 var yeoman = require('yeoman-generator');
 
-var Generator = yeoman.generators.Base.extend({
+var Generator = yeoman.Base.extend({
   compose: function() {
-    this.composeWith('ng-component:factory', {arguments: this.arguments}, { local: require.resolve('generator-ng-component/factory') });
+    this.composeWith('ng-component:factory', {arguments: this.arguments}, { local: require.resolve('generator-ng-component/generators/factory') });
   }
 });
 
