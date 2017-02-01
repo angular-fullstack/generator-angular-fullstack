af9cdb6e98f4a72d8e294e695a5749a8ac19198d
Nate Stuyvesant <nstuyvesant@users.noreply.github.com>
2017-02-01 10:28:49 -0500

    fix (app): prevent excess sync() calls during db seed (#2482)
    * fix app: prevent excess sync()s when seeding db
    
    Change app.js to seed database in promise chain before startServer, move seeding conditional to seed.js and wrap in function.
    
    * fix (app): prevent excess sync() calls during db seed
    
    Previously, .sync() was being called once in server/config/seed.js and once during sqldb.sequelize.sync() in server/app.js. As a result, duplicate queries were being sent to the database if the tables needed to be defined causing an error when it tries to define constraints a second time (like for an identity column).
    
    Seed.js is wrapped in a function and contains the conditional to determine whether to seed the database. This makes it easy to include in the promise chain under sqldb.sequelize.sync().
    
    * fix (app): prevent excess sync() calls during seed
    
    Previously, .sync() was being called once in server/config/seed.js and once during sqldb.sequelize.sync() in server/app.js. As a result, duplicate queries were being sent to the database if the tables needed to be defined causing an error when it tries to define constraints a second time (like for an identity column).
    
    Seed.js is wrapped in a function and contains the conditional to determine whether to seed the database. This makes it easy to include in the promise chain under sqldb.sequelize.sync().

--- templates/app/server/config/seed(models).js
+++ templates/app/server/config/seed(models).js
@@ -6,67 +6,70 @@
 'use strict';<% if (filters.mongooseModels) { %>
 import Thing from '../api/thing/thing.model';<% if (filters.auth) { %>
 import User from '../api/user/user.model';<% } %><% } %><% if (filters.sequelizeModels) { %>
-import sqldb from '../sqldb';
-var Thing = sqldb.Thing;<% if (filters.auth) { %>
-var User = sqldb.User;<% } %><% } %>
+import sqldb from '../sqldb';<% } %>
+import config from './environment/';
 
-<% if (filters.mongooseModels) { %>Thing.find({}).remove()<% }
-   if (filters.sequelizeModels) { %>Thing.sync()
-  .then(() => {
-    return Thing.destroy({ where: {} });
-  })<% } %>
-  .then(() => {
-    <% if (filters.mongooseModels) { %>Thing.create({<% }
-       if (filters.sequelizeModels) { %>Thing.bulkCreate([{<% } %>
-      name: 'Development Tools',
-      info: 'Integration with popular tools such as Webpack, Gulp, Babel, TypeScript, Karma, '
-            + 'Mocha, ESLint, Node Inspector, Livereload, Protractor, Pug, '
-            + 'Stylus, Sass, and Less.'
-    }, {
-      name: 'Server and Client integration',
-      info: 'Built with a powerful and fun stack: MongoDB, Express, '
-            + 'AngularJS, and Node.'
-    }, {
-      name: 'Smart Build System',
-      info: 'Build system ignores `spec` files, allowing you to keep '
-            + 'tests alongside code. Automatic injection of scripts and '
-            + 'styles into your index.html'
-    }, {
-      name: 'Modular Structure',
-      info: 'Best practice client and server structures allow for more '
-            + 'code reusability and maximum scalability'
-    }, {
-      name: 'Optimized Build',
-      info: 'Build process packs up your templates as a single JavaScript '
-            + 'payload, minifies your scripts/css/images, and rewrites asset '
-            + 'names for caching.'
-    }, {
-      name: 'Deployment Ready',
-      info: 'Easily deploy your app to Heroku or Openshift with the heroku '
-            + 'and openshift subgenerators'
-    <% if (filters.mongooseModels) { %>});<% }
-       if (filters.sequelizeModels) { %>}]);<% } %>
-  });
+export default function seedDatabaseIfNeeded() {
+  if(config.seedDB) {
+    <% if (filters.sequelizeModels) { %>let Thing = sqldb.Thing;<% if (filters.auth) { %>
+    let User = sqldb.User;<% } %><% } %>
+
+    <% if (filters.mongooseModels) { %>Thing.find({}).remove()<% }
+       if (filters.sequelizeModels) { %>return Thing.destroy({ where: {} })<% } %>
+      .then(() => {
+        <% if (filters.mongooseModels) { %>Thing.create({<% }
+           if (filters.sequelizeModels) { %>return Thing.bulkCreate([{<% } %>
+          name: 'Development Tools',
+          info: 'Integration with popular tools such as Webpack, Gulp, Babel, TypeScript, Karma, '
+                + 'Mocha, ESLint, Node Inspector, Livereload, Protractor, Pug, '
+                + 'Stylus, Sass, and Less.'
+        }, {
+          name: 'Server and Client integration',
+          info: 'Built with a powerful and fun stack: MongoDB, Express, '
+                + 'AngularJS, and Node.'
+        }, {
+          name: 'Smart Build System',
+          info: 'Build system ignores `spec` files, allowing you to keep '
+                + 'tests alongside code. Automatic injection of scripts and '
+                + 'styles into your index.html'
+        }, {
+          name: 'Modular Structure',
+          info: 'Best practice client and server structures allow for more '
+                + 'code reusability and maximum scalability'
+        }, {
+          name: 'Optimized Build',
+          info: 'Build process packs up your templates as a single JavaScript '
+                + 'payload, minifies your scripts/css/images, and rewrites asset '
+                + 'names for caching.'
+        }, {
+          name: 'Deployment Ready',
+          info: 'Easily deploy your app to Heroku or Openshift with the heroku '
+                + 'and openshift subgenerators'
+        <% if (filters.mongooseModels) { %>});<% }
+         if (filters.sequelizeModels) { %>}]);<% } %>
+    })
+    .then(() => console.log('finished populating things'))
+    .catch(err => console.log('error populating things', err));
 <% if (filters.auth) { %>
-<% if (filters.mongooseModels) { %>User.find({}).remove()<% }
-   if (filters.sequelizeModels) { %>User.sync()
-  .then(() => User.destroy({ where: {} }))<% } %>
-  .then(() => {
-    <% if (filters.mongooseModels) { %>User.create({<% }
-       if (filters.sequelizeModels) { %>User.bulkCreate([{<% } %>
-      provider: 'local',
-      name: 'Test User',
-      email: 'test@example.com',
-      password: 'test'
-    }, {
-      provider: 'local',
-      role: 'admin',
-      name: 'Admin',
-      email: 'admin@example.com',
-      password: 'admin'
-    <% if (filters.mongooseModels) { %>})<% }
-       if (filters.sequelizeModels) { %>}])<% } %>
-    .then(() => {
-      console.log('finished populating users');
+    <% if (filters.mongooseModels) { %>User.find({}).remove()<% }
+     if (filters.sequelizeModels) { %>User.destroy({ where: {} })<% } %>
+      .then(() => {
+        <% if (filters.mongooseModels) { %>User.create({<% }
+         if (filters.sequelizeModels) { %>return User.bulkCreate([{<% } %>
+          provider: 'local',
+          name: 'Test User',
+          email: 'test@example.com',
+          password: 'test'
+        }, {
+          provider: 'local',
+          role: 'admin',
+          name: 'Admin',
+          email: 'admin@example.com',
+          password: 'admin'
+        <% if (filters.mongooseModels) { %>})<% }
+         if (filters.sequelizeModels) { %>}])<% } %>
+        .then(() => console.log('finished populating users'))
+        .catch(err => console.log('error populating users', err));<% } %>
     });
-  });<% } %>
+  }
+}
