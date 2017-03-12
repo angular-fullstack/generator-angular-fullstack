9db9918573a52e0afe87461d0cfbd932fe24ccb9
stherrienaspnet <stherrienaspnet@gmail.com>
2017-03-12 17:48:01 -0400

    fix(angular-validation-match): Integration with Babel & Typescript (#2517)
    * Fix form field not displaying sequalize error
    
    * fix: Fix form field not displaying sequalize error
    
    fix: Fix form field not displaying sequalize error
    
    Display form field error when using sequelize as database
    
    * Fixed match integration for Babel & Typescript

--- templates/app/client/app/app.js
+++ templates/app/client/app/app.js
@@ -4,7 +4,7 @@ import angular from 'angular';
 import ngCookies from 'angular-cookies';
 import ngResource from 'angular-resource';
 import ngSanitize from 'angular-sanitize';
-import ngValidationMatch from 'angular-validation-match';
+
 <%_ if(filters.socketio) { _%>
 import 'angular-socket-io';<% } %>
 <%_ if(filters.ngroute) { _%>
@@ -15,6 +15,7 @@ import uiRouter from 'angular-ui-router';<% } %>
 import uiBootstrap from 'angular-ui-bootstrap';<% } %>
 // import ngMessages from 'angular-messages';
 <%_ if(filters.auth) { _%>
+import ngValidationMatch from 'angular-validation-match';
 <% } %>
 
 
@@ -39,7 +40,6 @@ angular.module('<%= scriptAppName %>', [
   ngCookies,
   ngResource,
   ngSanitize,
-  'validation.match',
   <%_ if(filters.socketio) { %>
   'btford.socket-io',<% } %>
   <%_ if(filters.ngroute) { %>
@@ -51,7 +51,9 @@ angular.module('<%= scriptAppName %>', [
   <%_ if(filters.auth) { %>
   _Auth,
   account,
-  admin,<% } _%>
+  admin,
+  'validation.match',
+  <% } _%>
   navbar,
   footer,
   main,
