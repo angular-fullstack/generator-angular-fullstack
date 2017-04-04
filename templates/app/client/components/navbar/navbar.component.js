cf971e0e79127db901f7e4eca95ce987e53914c9
stherrienaspnet <stherrienaspnet@gmail.com>
2017-04-04 13:49:14 -0400

    Imported navbar component.js

--- templates/app/client/components/navbar/navbar.component.js
+++ templates/app/client/components/navbar/navbar.component.js
@@ -4,11 +4,15 @@ import angular from 'angular';
 
 export class NavbarComponent {
   menu = [{
-    'title': 'Home',
+    <% if (filters.i18nSupport) { %>'title': 'HOME'<% } else { %>'title': 'Home'<% } %>,
     <% if (filters.uirouter) { %>'state': 'main'<% } else { %>'link': '/'<% } %>
   }];
   <%_ if(!filters.uirouter) { -%>
   $location;
+  <%_ } -%>
+  <%_ if(filters.i18nSupport) { -%>
+  $translate;
+  currentLocale;
   <%_ } -%>
   <%_ if (filters.auth) { -%>
   isLoggedIn: Function;
@@ -16,24 +20,40 @@ export class NavbarComponent {
   getCurrentUser: Function;
   <%_ } -%>
   isCollapsed = true;
-  <%_ if(filters.ngroute || filters.auth) { _%>
+  <%_ if(filters.ngroute || filters.auth || filters.i18nSupport) { _%>
 
-  constructor(<% if(!filters.uirouter) { %>$location<% } if(!filters.uirouter && filters.auth) { %>, <% } if (filters.auth) { %>Auth<% } %>) {
+  constructor(<% if(!filters.uirouter) { %>$location<% } if(!filters.uirouter && filters.auth) { %>, <% } 
+  if (filters.auth) { %>Auth<% } if (filters.i18nSupport && (!filters.uirouter || filters.auth)) { %>, <% } if (filters.i18nSupport) { %>$translate<% } %>) {  
+    
     'ngInject';
     <%_ if(!filters.uirouter) { _%>
     this.$location = $location;
     <%_ } _%>
+
+    <%_ if(filters.i18nSupport) { _%>
+    this.$translate = $translate; 
+    this.currentLocale = $translate.proposedLanguage(); 
+    <%_ } _%>
+
     <%_ if (filters.auth) { _%>
     this.isLoggedIn = Auth.isLoggedInSync;
     this.isAdmin = Auth.isAdminSync;
     this.getCurrentUser = Auth.getCurrentUserSync;
     <%_ } _%>
-  }<% } %>
-  <%_ if(!filters.uirouter) { _%>
 
+  }<% } %>
+
+  <%_ if(!filters.uirouter) { _%>
   isActive(route) {
     return route === this.$location.path();
   }<% } %>
+
+  <%_ if (filters.i18nSupport) { _%>
+  changeLocale(localeKey){ 
+    this.$translate.use(localeKey); 
+    this.currentLocale = localeKey; 
+  } 
+  <%_ } _%>
 }
 
 export default angular.module('directives.navbar', [])
