63f5c12b6d83e3afe7bf636358088c69020683a9
Andrew Koroluk <koroluka@gmail.com>
2017-04-29 17:10:53 -0400

    chore(gen:gulp): rm unused daux comand

--- gulpfile.js
+++ gulpfile.js
@@ -181,9 +181,6 @@ gulp.task('deps', () => console.log('TODO')); // updateFixtures, david
 gulp.task('release', () => console.log('TODO'));
 gulp.task('lint', () => console.log('TODO')); // ['gulpfile.js', 'src/**/*.js']
 
-gulp.task('daux', () => {
-    return execAsync('daux');
-});
 gulp.task('copy_docs_images', () => {
   return gulp.src('./media/svg/*')
     .pipe(gulp.dest('./static/'));
