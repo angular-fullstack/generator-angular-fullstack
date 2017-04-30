d71f890659140877018c713fa93fda48dee05954
Andrew Koroluk <koroluka@gmail.com>
2017-04-30 19:04:51 -0400

    test(client:oauth): fix oauth button test

--- templates/app/client/components/oauth-buttons(oauth)/oauth-buttons.component.spec.js
+++ templates/app/client/components/oauth-buttons(oauth)/oauth-buttons.component.spec.js
@@ -21,17 +21,45 @@ describe('Component: OauthButtonsComponent', () => {
         comp = fixture.componentInstance; // OauthButtonsComponent test instance
 
         // query for the title <h1> by CSS element selector
-        de = fixture.debugElement.query(By.css('a'));
-        el = de.nativeElement;
+        // de = fixture.debugElement.query(By.css('a'));
+        // el = de.nativeElement;
     });
 
-    it('test', () => {
-        console.log(fixture.debugElement.query(By.css('i.fa-google-plus')));
-        fixture.detectChanges();
-        <%_ if (filters.jasmine) { -%>
-        expect(el.textContent).toContain('Connect with Google+');
-        <%_ } if (filters.mocha) { -%>
-        <%= expect() %>el.textContent<%= to() %>.contain('Connect with Google+');
-        <%_ } -%>
+    <%_ if(filters.googleAuth) { -%>
+    it('should have a Google+ button', () => {
+      const a = fixture.debugElement.query(By.css('i.fa-google-plus')).parent.nativeElement;
+
+      fixture.detectChanges();
+
+      <%_ if (filters.jasmine) { -%>
+      expect(a.textContent).toContain('Connect with Google+');
+      <%_ } if (filters.mocha) { -%>
+      <%= expect() %>a.textContent<%= to() %>.contain('Connect with Google+');
+      <%_ } -%>
     });
+    <%_ } -%>
+
+    // it('should contain anchor buttons', function() {
+    //   compileDirective('<oauth-buttons></oauth-buttons>');
+    //   expect($(element[0]).find('a.btn.btn-social').length).to.be.at.least(1);
+    // });
+    //
+    // it('should evaluate and bind the classes attribute to scope.classes', function() {
+    //   parentScope.scopedClass = 'scopedClass1';
+    //   compileDirective('<oauth-buttons classes="testClass1 {{scopedClass}}"></oauth-buttons>');
+    //   expect(elementScope.classes).to.equal('testClass1 scopedClass1');
+    // });
+    //
+    // it('should bind scope.classes to class names on the anchor buttons', function() {
+    //   compileDirective('<oauth-buttons></oauth-buttons>');
+    //   // Add classes
+    //   elementScope.classes = 'testClass1 testClass2';
+    //   elementScope.$digest();
+    //   expect($(element[0]).find('a.btn.btn-social.testClass1.testClass2').length).to.be.at.least(1);
+    //
+    //   // Remove classes
+    //   elementScope.classes = '';
+    //   elementScope.$digest();
+    //   expect($(element[0]).find('a.btn.btn-social.testClass1.testClass2').length).to.equal(0);
+    // });
 });
