73d1e29c35b9b215bd17a55e6e5b603e910c7120
Andrew Koroluk <koroluka@gmail.com>
2018-07-16 03:17:11 -0700

    fix(client): hotfix for Babel bug

--- templates/app/client/app/app.constants.js
+++ templates/app/client/app/app.constants.js
@@ -1,1 +1,7 @@
-export * from '../../server/config/environment/shared';
+<% if(filters.ts) { %>export * from '../../server/config/environment/shared';<% } else { %>
+// https://github.com/babel/babel/issues/2877
+import * as shared from '../../server/config/environment/shared';
+
+export const env = shared.env;
+export const port = shared.port;
+export const userRoles = shared.userRoles;<% } %>
