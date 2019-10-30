77608cd5e9a4cf3614e1099d018a6b94aff5440b
Andrew Koroluk <koroluka@gmail.com>
2019-10-29 18:28:25 -0700

    feat: default to Puppeteer for Karma

--- templates/app/karma.conf.js
+++ templates/app/karma.conf.js
@@ -92,7 +92,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
